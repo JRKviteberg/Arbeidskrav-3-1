8c8c30cf57cb33cfccf12df72196d7be2e01bf73
Silje Elvik <silje.elvik.halvorsen@gmail.com>
2025-11-17 12:15:44 +0100

    js

--- JS/index.js
+++ JS/index.js
@@ -16,10 +16,56 @@ toggleBtn.addEventListener("click", () => {
   console.log("Dark mode toggled");
 });
 
-const logobutton = document.getElementById('beanbutton');
+const logobutton = document.getElementById("beanbutton");
 
 logobutton.addEventListener("click", growSprout);
 
 function growSprout() {
   alert("The logo was clicked!");
+}
+
+const monthlybundle = [
+  {
+    name: "Chai latte",
+    description:
+      "En varm og krydret drikk laget med sort te, melk og en blanding av krydder som kanel, kardemomme og ingefær.",
+    image: "assets/images/bunbundle.png",
+  },
+  {
+    name: "Pumpkin spice islatte",
+    description:
+      "En forfriskende kald drikk som kombinerer espresso, isbiter, melk og vår signatur gresskarkrydderblanding, toppet med pisket krem og et dryss av kanel.",
+    image: "assets/images/pumpinicedbunble.png",
+  },
+  {
+    name: "Pumpkin spice latte",
+    description:
+      "En kremet og smakfull høstfavoritt laget med espresso, steamet melk og vår signatur gresskarkrydderblanding, toppet med pisket krem og et dryss av kanel.",
+    image: "assets/images/pumpinspicelattebundle.png",
+  },
+];
+
+const container = document.getElementById("monthly-bundle");
+
+for (let i = 0; i < monthlybundle.length; i++) {
+  const items = monthlybundle[i];
+
+  const card = document.createElement("div");
+  card.classList.add("bundle-card");
+
+  card.innerHTML = `
+    <img src="${items.image}" alt="Bilde av ${items.name}" class="bundle-photo">
+    <h3>${items.name}</h3>
+    <p>${items.description}</p>
+    <button onclick="openOrder('${items.name}')">Bestill</button>
+  `;
+
+  container.appendChild(card);
+}
+
+function openContactForm(name) {
+  const message = prompt(`Skriv en melding til ${name}:`);
+  if (message) {
+    alert("Din melding er sendt!");
+  }
 }
